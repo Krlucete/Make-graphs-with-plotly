3ab7b4add3e1b91306a62db01ee55d6f287fbe86
Krlucete <Krlucete@gmail.com>
2018-09-17 10:00:26 +0900

    허준호교수님 요청사항 수정완료 1. mean값을 기준으로 Normalization 실행 2. 그래프에서 Max와 Min값은 점이 아닌 영역으로 표시

--- graph.R
+++ graph.R
@@ -4,13 +4,13 @@ setwd("/Users/lucas/Desktop/R/Make-graphs-with-plotly")
 csvData <- read.csv(file="combine.csv", header=TRUE, sep=",")
 csvData <- csvData[,2:8]
 
-totalMax <- sum(csvData$max)
+totalMean <- sum(csvData$mean)
 
 #Normalization of sum of max
 #Cumulative sums of max,min,mean
-csvData$cum_sum_max <- cumsum(csvData$max/totalMax)
-csvData$cum_sum_min <- cumsum(csvData$min/totalMax)
-csvData$cum_sum_mean <- cumsum(csvData$mean/totalMax)
+csvData$cum_sum_max <- cumsum(csvData$max/totalMean)
+csvData$cum_sum_min <- cumsum(csvData$min/totalMean)
+csvData$cum_sum_mean <- cumsum(csvData$mean/totalMean)
 
 library(plotly)
 RGEN <- csvData$RGEN
@@ -23,36 +23,39 @@ data$RGEN <- factor(data$RGEN, levels = data[["RGEN"]])
 zero_data <- data.frame(RGEN="NONE",cum_sum_max=0,cum_sum_min=0,cum_sum_mean=0)
 data <- rbind(zero_data,data)
 
-p <- plot_ly(data, x = ~RGEN, y = ~cum_sum_max, type = 'scatter', mode = 'markers',
-                          showlegend = FALSE, name = 'cum_sum_max') %>%
-               add_trace(y = ~cum_sum_min, type = 'scatter', mode = 'markers',
+p <- plot_ly(data, x = ~RGEN, y = ~cum_sum_max, type = 'scatter', mode = 'lines',
+             line = list(color = 'transparent'),    
+             showlegend = TRUE, name = 'cum_sum_max') %>%
+               add_trace(y = ~cum_sum_min, type = 'scatter', mode = 'lines',
+                         line = list(color = 'transparent'),
                          fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)',
-                         showlegend = FALSE, name = 'cum_sum_min') %>%
+                         showlegend = TRUE, name = 'cum_sum_min') %>%
                add_trace(x = ~RGEN, y = ~cum_sum_mean, type = 'scatter', mode = 'markers',
                          name = 'cum_sum_mean') %>%
                layout(title = "Mean, High and Low Count of RGEN",
                       paper_bgcolor='rgb(255,255,255)', plot_bgcolor='rgb( m229,229,229)',
                       xaxis = list(title = "RGEN",
-                                   gridcolor = 'rgb(255,255,255)',
+                                   autotick=TRUE,
+                                   gridcolor = 'rgb(229,229,229)',
                                    showgrid = TRUE,
-                                   showline = FALSE,
+                                   showline = TRUE,
                                    showticklabels = TRUE,
                                    tickcolor = 'rgb(127,127,127)',
                                    ticks = 'outside',
-                                   zeroline = FALSE),
+                                   zeroline = TRUE),
                       yaxis = list(title = "Normalization of Count",
                                    autotick=FALSE,
                                    tick0=0,
                                    dtick=0.05,
                                    tickwidth=1,
-                                   gridcolor = 'rgb(255,255,255)',
+                                   gridcolor = 'rgb(229,229,229)',
                                    showgrid = TRUE,
-                                   showline = FALSE,
+                                   showline = TRUE,
                                    showticklabels = TRUE,
                                    tickcolor = 'rgb(127,127,127)',
                                    ticks = 'outside',
-                                   zeroline = FALSE))
+                                   zeroline = TRUE))
 p
 
 write.csv(csvData, file="graph.csv",row.names=TRUE)
-
+ 
