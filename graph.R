38bce4ea6dec9b09edd852bbd090c827f10bc468
Krlucete <Krlucete@gmail.com>
2018-10-01 14:59:49 +0900

    error_y 를 이용해서 stdev 표시 mean과 stdev로 그래프 그리기

--- graph.R
+++ graph.R
@@ -2,38 +2,33 @@ setwd("/Users/lucas/Desktop/R/Make-graphs-with-plotly")
 
 #FILE input
 csvData <- read.csv(file="combine.csv", header=TRUE, sep=",")
-csvData <- csvData[,2:11]
+csvData <- csvData[,2:12]
 
 totalMean <- sum(csvData$mean)
 
 #Normalization of sum of max
-#Cumulative sums of max,min,mean
-csvData$cum_sum_max <- cumsum(csvData$max/totalMean)
-csvData$cum_sum_min <- cumsum(csvData$min/totalMean)
 csvData$cum_sum_mean <- cumsum(csvData$mean/totalMean)
 
 library(plotly)
 RGEN <- csvData$RGEN
-cum_sum_max <- csvData$cum_sum_max
-cum_sum_min <- csvData$cum_sum_min
 cum_sum_mean <- csvData$cum_sum_mean
+stdev <- (csvData$stdev)/totalMean
 
-data <- data.frame(RGEN,cum_sum_max,cum_sum_min,cum_sum_mean)
+data <- data.frame(RGEN,cum_sum_mean,stdev)
 data$RGEN <- factor(data$RGEN, levels = data[["RGEN"]])
-zero_data <- data.frame(RGEN="NONE",cum_sum_max=0,cum_sum_min=0,cum_sum_mean=0)
+zero_data <- data.frame(RGEN="NONE",cum_sum_mean=0,stdev=0)
 data <- rbind(zero_data,data)
 
-p <- plot_ly(data, x = ~RGEN, y = ~cum_sum_max, type = 'scatter', mode = 'lines',
-             line = list(color = 'transparent'),    
+p <- plot_ly(data, 
+             x = ~RGEN, 
+             y = ~cum_sum_mean, 
+             type = 'scatter',
+             error_y = list(type = "data", array = ~stdev, color='rgba(243,129,129,1)'), 
+             line = list(color = 'transparent'),
              showlegend = TRUE, name = 'cum_sum_max') %>%
-               add_trace(y = ~cum_sum_min, type = 'scatter', mode = 'lines',
-                         line = list(color = 'transparent'),
-                         fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)',
-                         showlegend = TRUE, name = 'cum_sum_min') %>%
-               add_trace(x = ~RGEN, y = ~cum_sum_mean, type = 'scatter', mode = 'markers',
-                         name = 'cum_sum_mean') %>%
                layout(title = "Mean, High and Low Count of RGEN",
-                      paper_bgcolor='rgb(255,255,255)', plot_bgcolor='rgb( m229,229,229)',
+                      paper_bgcolor='rgb(255,255,255)',
+                      plot_bgcolor='rgb( m229,229,229)',
                       xaxis = list(title = "RGEN",
                                    autotick=TRUE,
                                    gridcolor = 'rgb(229,229,229)',
@@ -55,7 +50,10 @@ p <- plot_ly(data, x = ~RGEN, y = ~cum_sum_max, type = 'scatter', mode = 'lines'
                                    tickcolor = 'rgb(127,127,127)',
                                    ticks = 'outside',
                                    zeroline = TRUE))
+
 p
 
 write.csv(csvData, file="graph.csv",row.names=TRUE)
- 
+
+
+
